fc2ad2771f07f4f714c24a6ddf10b5a25818c977
Iryna Artymuk <artimyk@ukr.net>
2022-12-04 15:18:09 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,28 +1,35 @@
 const ref = {
-    form: document.querySelector('.login-form'),
-    btnLogin:document.querySelector('button ')
+  form: document.querySelector(".login-form"),
+  btnLogin: document.querySelector("button "),
+};
+
+ref.form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const userEmail = email.value;
+  const userPassword = password.value;
+
+  if (email.value === "" || password.value === "") {
+    return alert` Please,fill up all information before login `;
+  }
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    console.log(name, value);
+  });
+
+  const formData2 = {
+    userEmail,
+    userPassword,
+  };
+
+  // console.log(userEmail)
+  // console.log(userPassword)
+
+  console.log(formData);
+  console.log(formData2);
 }
-
-ref.form.addEventListener('submit', onSubmit)
-
-const formData={}
-function onSubmit(event){
-    event.preventDefault()
-    // console.dir(event.currentTarget.elements)
-   const  {elements: {email,password}} = event.currentTarget
-    // console.log(email.value)
-    // console.log(password.value)
-
-    if (email.value && password.value !== "") {
-        formData.userEmail=`${email.value}`
-        formData.userpassword=`${password.value}`
-       
-    }
-    else {
-        alert` Please,fill up all information before login `
-    
-    }
-
-}
-
-console.log(formData)
