d5459cd2de32688ceea099fdb4c893e69cf70728
Iryna Artymuk <artimyk@ukr.net>
2022-12-04 15:42:21 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  body: document.querySelector("body"),
+  button: document.querySelector(".change-color"),
+  text: document.querySelector(".color"),
+};
+
+ref.button.addEventListener("click", changeColorOnClick);
+
+function changeColorOnClick(event) {
+  const newColor = getRandomHexColor();
+  ref.body.style.backgroundColor = `${newColor}`;
+  ref.text.textContent = `${newColor}`;
+}
