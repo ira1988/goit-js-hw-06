43f9879daf8216638fecb2dbfca19d87103ac6ce
Iryna Artymuk <artimyk@ukr.net>
2022-12-04 21:47:48 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,53 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const ref={
+  input:document.querySelector('input'),
+  btnCreate:document.querySelector('[data-create]'),
+  btnDelete:document.querySelector('[data-destroy]'),
+  boxes:document.querySelector('#boxes'),
+  
+}
+
+let amount =0;
+
+ref.input.addEventListener('input',getAmountOnInput)
+function getAmountOnInput(event){
+amount= event.currentTarget.value
+}
+
+ref.btnCreate.addEventListener('click', createBox)
+
+function createBox( ){
+
+
+  const boxMarkup  =[]
+  for(let i=1;i<=amount;i+=1){
+  let  width = 30;
+  const color= getRandomHexColor();
+    boxMarkup.push(`<div style="width:${width}px;height:${width}px;background:${color}"></div>`
+
+    )
+  }
+  const boxMarkupString=boxMarkup.join(',')
+  console.log(boxMarkupString)
+  ref.boxes.insertAdjacentHTML('beforeend',boxMarkupString)
+  }
+
+
+
+ref.btnDelete.addEventListener('click', clearPage)
+
+function clearPage(){
+
+  ref.boxes.innerHTML = "" ;
+}
+
+
+
+
+
+
+
