733a2ebdafedad95b9a94741b9e5e5fd30f8dc76
Iryna Artymuk <artimyk@ukr.net>
2022-12-05 15:51:25 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,54 +2,41 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const ref = {
+  input: document.querySelector("input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDelete: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
 
-const ref={
-  input:document.querySelector('input'),
-  btnCreate:document.querySelector('[data-create]'),
-  btnDelete:document.querySelector('[data-destroy]'),
-  boxes:document.querySelector('#boxes'),
-  
+let amount = 0;
+
+ref.input.addEventListener("input", getAmountOnInput);
+function getAmountOnInput(event) {
+  amount = event.currentTarget.value;
 }
 
-let amount =0;
+ref.btnCreate.addEventListener("click", createBox);
 
-ref.input.addEventListener('input',getAmountOnInput)
-function getAmountOnInput(event){
-amount= event.currentTarget.value
+function createBox() {
+  const boxMarkup = [];
+  for (let i = 1; i <= amount; i += 1) {
+    let width = 30 + i * 10;
+    const color = getRandomHexColor();
+    boxMarkup.push(
+      `<div class=".box__item" style="width:${width}px;height:${width}px;background:${color}"></div>`
+    );
+  }
+  const boxMarkupString = boxMarkup.join("");
+  console.log(boxMarkupString);
+  ref.boxes.insertAdjacentHTML("beforeend", boxMarkupString);
+  ref.boxes.classList.add("box-contaiten");
 }
 
-ref.btnCreate.addEventListener('click', createBox)
+ref.btnDelete.addEventListener("click", clearPage);
 
-function createBox( ){
-
-
-  const boxMarkup  =[]
-  for(let i=1;i<=amount;i+=1){
-  let  width = 30+i*10;
-  const color= getRandomHexColor();
-    boxMarkup.push(`<div class=".box__item" style="width:${width}px;height:${width}px;background:${color}"></div>`
-
-    )
-  }
-  const boxMarkupString=boxMarkup.join('')
-  console.log(boxMarkupString)
-  ref.boxes.insertAdjacentHTML('beforeend', boxMarkupString)
-  ref.boxes.classList.add('box-contaiten')
-  }
-
-
-
-ref.btnDelete.addEventListener('click', clearPage)
-
-function clearPage(){
+function clearPage() {
   ref.boxes.innerHTML = "";
 
-  ref.input.value =""
+  ref.input.value = "";
 }
-
-
-
-
-
-
-
